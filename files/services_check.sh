637574b4518b62bdc60c17f2055854f2b63f5559
igor71 <34439207+igor71@users.noreply.github.com>
2018-08-28 09:34:09 +0300

    Update and rename start.sh to services_check.sh

---
+++ files/services_check.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check the status of sshd service and start it if not running
+
+case "$(pidof sshd | wc -w)" in
+
+0)  echo "RESTARTING SSHD SERVICE:"
+    /etc/init.d/ssh start
+    ;;
+1)  echo "SSHD SERVICE RUNNING"
+    ;;
+esac
+
+# Check the status of tomcat service and start it if not running
+
+TOMCAT_PID=$(ps -ef | awk '/[t]omcat/{print $2}')
+
+if [ -z "$TOMCAT_PID" ]
+then
+    echo "TOMCAT NOT RUNNING"
+          sudo /opt/tomcat/bin/startup.sh
+else
+    echo "TOMCAT RUNNING"
+fi
